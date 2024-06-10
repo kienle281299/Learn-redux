3d083d5afd67790d8166e054e04100d47221fb46
kienle2812 <kienl3142857@gmail.com>
2024-06-10 09:54:10 +0700

    push code redux thunk

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,27 +1,80 @@
-import React from 'react';
+// import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { addTodo, removeTodo } from './todoSlice';
+
+// const TodoList = () => {
+  
+//     const todos = useSelector((state) => state.todos.todos);
+//     const dispatch = useDispatch();
+//     console.log(todos);
+
+//   const handleAddTodo = () => {
+//     const text = prompt('Enter todo:');
+//     dispatch(addTodo(text));
+//   };
+
+//   return (
+//     <div>
+//         <h1>Redux-Toolkit</h1>
+//       <button onClick={handleAddTodo}>Add Todo</button>
+//       <ul>
+//         {todos.map((todo, index) => (
+//           <li key={index}>
+//             {todo.text}
+//             <button onClick={() => dispatch(removeTodo(index))}>Remove</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default TodoList;
+// ==========
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addTodo, removeTodo } from './todoSlice';
+import { fetchTodos, addNewTodo, deleteTodo } from './todoSlice';
 
 const TodoList = () => {
-  
-    const todos = useSelector((state) => state.todos.todos);
-    const dispatch = useDispatch();
-    console.log(todos);
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos.todos);
+  const status = useSelector((state) => state.todos.status);
+  const error = useSelector((state) => state.todos.error);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchTodos());
+    }
+  }, [status, dispatch]);
 
   const handleAddTodo = () => {
     const text = prompt('Enter todo:');
-    dispatch(addTodo(text));
+    if (text.trim()) {
+      dispatch(addNewTodo(text.trim()));
+    }
   };
+
+  const handleDeleteTodo = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div>
-        <h1>Redux-Toolkit</h1>
+      <h1>Todo List</h1>
       <button onClick={handleAddTodo}>Add Todo</button>
       <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
+        {todos.map((todo) => (
+          <li key={todo.id}>
             {todo.text}
-            <button onClick={() => dispatch(removeTodo(index))}>Remove</button>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
           </li>
         ))}
       </ul>
@@ -30,3 +83,4 @@ const TodoList = () => {
 };
 
 export default TodoList;
+
--- src/todoSlice.js
+++ src/todoSlice.js
@@ -1,35 +1,173 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+// import { createSlice } from '@reduxjs/toolkit';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// export const fetchTodos = createAsyncThunk(
+//     'todos/fetchTodos',
+//     async () => {
+//       const response = await fetch('http://localhost:4000/todos');
+//       const data = await response.json();
+//       return data;
+//     }
+//   );
+
+// const todoSlice = createSlice({
+//   name: 'todos',
+//   initialState: {
+//     todos: [] // Thêm todos vào initialState như một mảng
+//   },
+//   reducers: {
+//     addTodo: (state, action) => {
+//       state.todos.push({ text: action.payload, completed: false });
+//     },
+//     removeTodo: (state, action) => {
+//         state.todos = state.todos.filter((_, index) => index !== action.payload);
+//     }
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchTodos.fulfilled, (state, action) => {
+//         state.todos = action.payload;
+//       });
+//   },
+// });
+
+// export const { addTodo, removeTodo } = todoSlice.actions;
+// export default todoSlice.reducer;
+// ==================================
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+// export const fetchTodos = createAsyncThunk(
+//   'todos/fetchTodos',
+//   async () => {
+//     const response = await fetch('http://localhost:4000/todos');
+//     const data = await response.json();
+//     return data;
+//   }
+// );
+
+// export const addNewTodo = createAsyncThunk(
+//   'todos/addNewTodo',
+//   async (text) => {
+//     const response = await fetch('http://localhost:4000/todos', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ text, completed: false }),
+//     });
+//     const data = await response.json();
+//     return data;
+//   }
+// );
+
+// export const deleteTodo = createAsyncThunk(
+//   'todos/deleteTodo',
+//   async (id) => {
+//     await fetch(`http://localhost:4000/todos/${id}`, {
+//       method: 'DELETE',
+//     });
+//     return id;
+//   }
+// );
+
+// const todoSlice = createSlice({
+//   name: 'todos',
+//   initialState: {
+//     todos: [],
+//     status: 'idle',
+//     error: null,
+//   },
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchTodos.pending, (state) => {
+//         state.status = 'loading';
+//       })
+//       .addCase(fetchTodos.fulfilled, (state, action) => {
+//         state.status = 'succeeded';
+//         state.todos = action.payload;
+//       })
+//       .addCase(fetchTodos.rejected, (state, action) => {
+//         state.status = 'failed';
+//         state.error = action.error.message;
+//       })
+//       .addCase(addNewTodo.fulfilled, (state, action) => {
+//         state.todos.push(action.payload);
+//       })
+//       .addCase(deleteTodo.fulfilled, (state, action) => {
+//         state.todos = state.todos.filter(todo => todo.id !== action.payload);
+//       });
+//   },
+// });
+
+// export default todoSlice.reducer;
+// ====
+
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchTodos = createAsyncThunk(
-    'todos/fetchTodos',
-    async () => {
-      const response = await fetch('http://localhost:4000/todos');
-      const data = await response.json();
-      return data;
-    }
-  );
+  'todos/fetchTodos',
+  async () => {
+    const response = await fetch('http://localhost:4000/todos');
+    const data = await response.json();
+    return data;
+  }
+);
+
+export const addNewTodo = createAsyncThunk(
+  'todos/addNewTodo',
+  async (text) => {
+    const response = await fetch('http://localhost:4000/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text, completed: false }),
+    });
+    const data = await response.json();
+    return data;
+  }
+);
+
+export const deleteTodo = createAsyncThunk(
+  'todos/deleteTodo',
+  async (id) => {
+    await fetch(`http://localhost:4000/todos/${id}`, {
+      method: 'DELETE',
+    });
+    return id;
+  }
+);
 
 const todoSlice = createSlice({
   name: 'todos',
   initialState: {
-    todos: [] // Thêm todos vào initialState như một mảng
+    todos: [],
+    status: 'idle',
+    error: null,
   },
-  reducers: {
-    addTodo: (state, action) => {
-      state.todos.push({ text: action.payload, completed: false });
-    },
-    removeTodo: (state, action) => {
-        state.todos = state.todos.filter((_, index) => index !== action.payload);
-    }
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
+      .addCase(fetchTodos.pending, (state) => {
+        state.status = 'loading';
+      })
       .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.status = 'succeeded';
         state.todos = action.payload;
+      })
+      .addCase(fetchTodos.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(addNewTodo.fulfilled, (state, action) => {
+        state.todos.push(action.payload);
+      })
+      .addCase(deleteTodo.fulfilled, (state, action) => {
+        state.todos = state.todos.filter(todo => todo.id !== action.payload);
       });
   },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
 export default todoSlice.reducer;
+
